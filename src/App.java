62a0e5063d8f24914293f33e570b22c0e3115ea0
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-01-14 17:01:34 +0100

    aggiunto bonus

--- src/App.java
+++ src/App.java
@@ -2,12 +2,27 @@ import org.ElencoInteri;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] valori = {1,2,3,4,5};
+        int[] valori = { 1, 2, 3, 4, 5 };
 
         ElencoInteri elenco = new ElencoInteri(valori);
 
-        while(elenco.hasAncoraElementi()){
-            System.out.println(elenco.getElementoSuccessivo()); 
+        elenco.addElemento(6);
+        elenco.addElemento(7);
+        elenco.addElemento(8);
+
+        while (elenco.hasAncoraElementi()) {
+            System.out.println(elenco.getElementoSuccessivo());
+        }
+
+        System.out.println("------------------");
+
+        ElencoInteri elenco2 = new ElencoInteri();
+        elenco2.addElemento(10);
+        elenco2.addElemento(20);
+        elenco2.addElemento(30);
+
+        while (elenco2.hasAncoraElementi()) {
+            System.out.println(elenco2.getElementoSuccessivo());
         }
 
     }
--- src/org/ElencoInteri.java
+++ src/org/ElencoInteri.java
@@ -5,13 +5,27 @@ public class ElencoInteri {
     private int[] numeri;
     private int indice = 0;
 
-
-    public ElencoInteri(int[] array){
+    public ElencoInteri(int[] array) {
         numeri = array;
     }
 
+    public ElencoInteri() {
+        numeri = new int[0];
 
-    public int getElementoSuccessivo(){
+    }
+
+    public void addElemento(int nuovoElemento) {
+
+        int[] nuovoArray = new int[numeri.length + 1];
+
+        for (int i = 0; i < numeri.length; i++) {
+            nuovoArray[i] = numeri[i];
+        }
+        nuovoArray[nuovoArray.length - 1] = nuovoElemento;
+        numeri = nuovoArray;
+    }
+
+    public int getElementoSuccessivo() {
 
         int num;
         num = numeri[indice];
@@ -20,9 +34,9 @@ public class ElencoInteri {
 
     }
 
-    public boolean hasAncoraElementi(){
+    public boolean hasAncoraElementi() {
 
-        return indice < numeri.length; 
+        return indice < numeri.length;
 
     }
 
